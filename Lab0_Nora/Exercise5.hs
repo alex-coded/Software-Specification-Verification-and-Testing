6e708435156ed28d1490b007b17a2d65b72ba409
norasilven <norasilven@gmail.com>
2023-09-07 20:33:36 +0100

    Ex 5 fixed?

--- Lab0_Nora/Exercise5.hs
+++ Lab0_Nora/Exercise5.hs
@@ -4,5 +4,14 @@ import System.Random
 import Test.QuickCheck
 
 rot13 :: [Char] -> [Char]
-rot13 [x] = [chr ((ord x) + 13)]
-rot13 (x:y) = [chr ((ord x) + 13)] ++ (rot13 y)
+rot13 [] = []
+rot13 [x] | elem x ['a'..'z'] = [chr ((rem ((ord x) - (ord 'a') + 13) 26) + ord 'a')]
+          | elem x ['A'..'Z'] = [chr ((rem ((ord x) - (ord 'A') + 13) 26) + ord 'A')]
+          | otherwise = [x]
+rot13 (x:y) = (rot13 [x]) ++ (rot13 y)
+
+prob_1 x = (rot13 (rot13 x)) == x
+
+main :: IO ()
+main = do
+    quickCheck prob_1
