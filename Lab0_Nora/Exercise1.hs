efa44e59127cbe33d90edb75338cee2d4c8e3c11
norasilven <norasilven@gmail.com>
2023-09-07 14:08:21 +0100

    Using generators in exercise 1 now

--- Lab0_Nora/Exercise1.hs
+++ Lab0_Nora/Exercise1.hs
@@ -3,19 +3,39 @@ import Data.Char
 import System.Random
 import Test.QuickCheck
 
+-- To calculate the left side of the first equation, I use the map function on a
+-- list from 1 to n to square all numbers. Then I use sum to add all elements together
 squares :: Int -> Int
-squares 1 = 1
-squares n = n^2 + (squares (n - 1))
+squares 0 = 0
+squares n = sum (map (\ x -> x^2) [1..n])
 
+-- This function calculates the right side of the first equation
 eq1 :: Int-> Int
-eq1 n = n * (n+1) * (div (2*n+1) 6)
+eq1 n = div (n * (n+1) * (2*n+1)) 6
 
+-- To calculate the left side of the second equation, I use the map function on a
+-- list from 1 to n to perform the power operation on all numbers. Then I sum all elements.
 cubics :: Int -> Int
-cubics 1 = 1
-cubics n = n^3 + (cubics(n - 1))
+cubics 0 = 0
+cubics n = sum (map (\ x -> x^3) [1..n])
 
+-- This function calculates the right side of the second equation.
 eq2 :: Int -> Int
-eq2 n = (n * (div (n+1) 2))^2
+eq2 n = (div (n * (n+1)) 2)^2
 
+-- This generator is used to generate numbers greater than or equal to 0, in order
+-- to test the equations accurately.
+posGen :: Gen Int
+posGen = suchThat (arbitrary :: Gen Int) (>= 0)
+
+-- For both equations I exclude negative numbers. Then I check if both sides of
+-- the equation are equal.
 prop_eq1 n = squares(n) == eq1(n)
 prop_eq2 n = cubics(n) == eq2(n)
+
+main :: IO ()
+main = do
+    quickCheck $ forAll posGen $ prop_eq1
+    quickCheck $ forAll posGen $ prop_eq2
+
+-- To prove that these statements are true I would use induction.
--- Lab0_Nora/Exercise4.hs
+++ Lab0_Nora/Exercise4.hs
@@ -13,14 +13,32 @@ primes = 2 : filter prime [3..]
 reversal :: Integer -> Integer
 reversal = read . reverse . show
 
+-- Here, I take numbers from the primes stream, as long as they are less than 10000.
+-- From these numbers, I filter out the numbers of which the reverse is not prime.
+-- So the resulting list only contains prime numbers under 10000 of which the reverse
+-- is also prime.
 reversibleStream :: [Integer]
 reversibleStream = filter (\ x -> prime (reversal x)) (takeWhile (<10000) primes)
 
+-- This function determines if all elements in a list are unique, used for property 6.
+-- I recursively check for every element if they are also present in the rest of the list.
+-- By using the && operator, every element has to be unique for the result to be True.
 unique :: [Integer] -> Bool
 unique [] = True
 unique (x:y) = notElem x y && unique y
 
-prop_1 x = (x >= 0) && reversal (reversal x) == x
+-- The reversal function does not work on negative numbers or numbers ending on a 0.
+-- That is why I filter out those numbers when testing the reversal function. I feel
+-- like ignoring these numbers is OK since none of these numbers are prime.
+prop_1 x = if (x >= 0) && (rem x 10 /= 0) then reversal (reversal x) == x else True
+
+-- For the prime membership property, I simply check whether all elements in the
+-- list are prime.
 prop_3 = all prime reversibleStream
+
+-- For the uniqueness property, I use the unique function defined above to check
+-- whether all elements in the list are unique.
 prop_6 = unique reversibleStream
+
+-- For the final property, I check if all elements in the list are below 10000.
 prop_7 = all (< 10000) reversibleStream
