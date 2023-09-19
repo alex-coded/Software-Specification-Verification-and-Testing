f7195b16ceb97167f153213b13a9a66ef3bd76da
norasilven <norasilven@gmail.com>
2023-09-19 21:27:13 +0100

    Added comment report for ex 8

--- Lab1_Nora/Exercise8.hs
+++ Lab1_Nora/Exercise8.hs
@@ -1,9 +1,5 @@
 module Exercise8 where
 
-import Data.List
-import System.Random
-import Test.QuickCheck
-
 -- This function was given in Lab0 and returns True when the input is a prime number
 -- and False otherwise.
 prime :: Integer -> Bool
@@ -15,9 +11,9 @@ prime n = n > 1 && all (\ x -> rem n x /= 0) xs
 primes :: [Integer]
 primes = 2 : filter prime [3..]
 
--- This function generages a list of tuples, containing a list of consecutive primes
--- and the result of multiplying that list and adding 1. Such a tuple only makes it
--- into the list if the result is not prime.
+-- This function generates a list of tuples, containing a list of consecutive primes
+-- and the result of multiplying all the elements in that list and adding 1. Such a
+-- tuple only makes it into the list if the result is not prime.
 counterexamples :: [([Integer], Integer)]
 counterexamples = findCounters 2
 
@@ -25,7 +21,7 @@ counterexamples = findCounters 2
 -- from 2 of length n. If the result of multiplying all primes in the list and adding 1
 -- is not prime, a tuple containing the list of n primes, and the result of the
 -- calculation is added to the resulting list. This process is repeated for every integer
--- starting from n.
+-- starting from the initial input n.
 findCounters :: Int -> [([Integer], Integer)]
 findCounters n | prime result = findCounters (n+1)
                | otherwise = solution ++ findCounters (n+1)
@@ -33,7 +29,7 @@ findCounters n | prime result = findCounters (n+1)
                      result = (product lst) + 1
                      solution = [(lst, result)]
 
-main :: IO ()
+main :: [([Integer], Integer)]
 main = do
     counterexamples
 
@@ -41,14 +37,28 @@ main = do
 {-
 --------- LIBRARIES ---------
 
-
+No additional libraries are used in this Haskell program.
 
 --------- CODE DESCRIPTION (PURPOSE, FEATURES, ARCHITECTURE) ---------
 
-
+The purpose of this code was to disprove the following statement:
+"If p1,...,pn is a list of consecutive primes starting from 2, then (p1×...×pn)+1
+is also prime."
+This is done by generating a list of counterexamples to this statement. This was done
+by incrementally taking a list of consecutive primes and checking if the outcome of the
+calculation is indeed prime. So first, check for [2, 3], then [2, 3, 5], then [2, 3, 5, 7] etc.
+When the outcome is NOT prime, it is a counterexample for the statement and it is added to
+the resulting list of counterexamples.
 
 --------- TESTING APPROACH ---------
 
+No testing is required for this exercise, but to check the implementation of the counterexamples
+function, I manually checked the first 10 counterexamples, which were indeed counterexamples.
+If I had to write quickCkeck tests for this exercise, I would have taken the first 20 elements
+of the list to test on, and I would have tested the following properties:
 
+- Whether every 2nd element in the tuples is not prime
+- Whether the list in every tuple contains consecutive tuples starting with 2
+- Whether the 2nd element is actually the result of the calculation described above
 
 -}
