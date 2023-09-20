4ce533adbfff5b0f37d5313528e31f89e29d0686
Alexandra Volentir <67002616+ada-boost@users.noreply.github.com>
2023-09-20 18:01:19 +0200

    update ex 5

--- Lab1_Alexandra/Exercise5.hs
+++ Lab1_Alexandra/Exercise5.hs
@@ -1,38 +1,45 @@
+-- import System.Posix.Fcntl (Advice(AdviceRandom))
+import Data.List (permutations)
 data Boy = Matthew | Peter | Jack | Arnold | Carl
     deriving (Eq,Show)
 boys = [Matthew, Peter, Jack, Arnold, Carl]
 
--- main :: IO ()
--- main = do
---     undefined
---     -- print Boy
 
 accuses :: Boy -> Boy -> Bool
-accuses Matthew Carl = False
-accuses Matthew Matthew = False
-accuses Peter b = b == Matthew || 
--- accuses Jack Peter = True
--- accuses Jack Matthew = True
--- accuses Carl Arnold = True
+accuses Matthew b = not (b == Carl || b == Matthew)
+accuses Peter b = b == Matthew || b == Jack
+accuses Jack b = not (accuses Matthew b) && not (accuses Peter b)
+accuses Arnold b = (accuses Matthew b && not (accuses Peter b)) || (accuses Peter b && not (accuses Matthew b))
+accuses Carl b = not (accuses Arnold b)
 
 accusers :: Boy -> [Boy]
-accusers Matthew = [Peter, Jack, Arnold]
-accusers Peter = []
-accusers Jack = []
-accusers Arnold = []
-accusers Carl = []
+accusers b = [x | x <- boys, accuses x b]
 
+-- accusers Matthew = [Peter, Arnold]
+-- accusers Peter = [Matthew, Arnold]
+-- accusers Jack = [Matthew, Peter, Carl]
+-- accusers Arnold = [Matthew, Arnold]
+-- accusers Carl = [Jack, Carl]
 
-
-
-
--- guilty, honest :: [Boy]
--- guilty 
-
-
-
+main :: IO ()
+main = do
+    let res = filter (\x -> length x == 3) $ map accusers boys
+    print res
 
 
 
 -- if the puzzle is well-designed, 
 -- then guilty should give a singleton list.
+
+
+{-
+
+--------- LIBRARIES ---------
+
+We used libr
+
+--------- CODE DESCRIPTION (PURPOSE, FEATURES, ARCHITECTURE) ---------
+
+--------- TESTING APPROACH ---------
+
+-}
