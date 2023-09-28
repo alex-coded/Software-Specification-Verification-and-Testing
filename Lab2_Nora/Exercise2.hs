56d2c6db58af26cd0160fa2f3875855cc2fcbf20
norasilven <norasilven@gmail.com>
2023-09-28 14:21:10 +0200

    ex 2

--- Lab2_Nora/Exercise2.hs
+++ Lab2_Nora/Exercise2.hs
@@ -4,17 +4,20 @@ import Data.List
 import LTS
 import Test.QuickCheck
 
+transitionGen :: Gen (State, Label, State)
+transitionGen = do
+    -- Generate a transition
+    k <- chooseInteger(1, 20)
+    s <- chooseInteger(1, k) -- State before transition
+    t <- suchThat (chooseInteger(1, k)) (/= s) -- state after transition
+    -- Choose 1 label from a list of labels
+    l <- elements ([tau] ++ (map (\ x -> if mod x 2 == 0 then "?" ++ (show x) else "!" ++ (show x)) [1..k]))
+    return (s, l, t)
+
 ltsGen :: Gen IOLTS
 ltsGen = do
-    -- Generate states and starting state
     k <- suchThat getSize (< 20)
-    n1 <- chooseInteger(1, k)
-    q0 <- chooseInteger(1, n1)
-    -- Generate input and output labels
-    k <- suchThat getSize (< 20)
-    n2 <- choose(1, k)
-    li <- map (\ x -> ["?"] ++ (show x)) [1..n2]
-    lu <- map (\ x -> ["!"] ++ (show x)) [1..n2]
-    -- Generate transitions
-    t <- [(s, l, t) | s <- shuffle [1..n1], l <- take n1::Int (shuffle (li ++ lu ++ [tau])), t <- shuffle [1..n1]]
-    return ([1..n1], li, lu, t, q0)
+    n <- choose(1, k)
+    transitions <- vectorOf n transitionGen
+    return (createIOLTS transitions)
+
