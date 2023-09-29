1af0547bb4844ec71e219be5ffc2e87a565de59a
dasha12678 <daria.protsenkoo@gmail.com>
2023-09-29 23:22:08 +0200

    Update Exercise4.hs

--- Lab2_Dasha/Exercise4.hs
+++ Lab2_Dasha/Exercise4.hs
@@ -1,6 +1,6 @@
 -- Study: MSc Software Engineering.
 -- This program is intended to implement and test the function after for IOLTS. 
--- Time spent: 1h
+-- Time spent: too loong
 
 module Exercise4 where
 
@@ -9,7 +9,6 @@ import Data.List
 import LTS
 import Test.QuickCheck
 import Exercise2
---import Exercise3
 
 {-
 
@@ -88,11 +87,35 @@ In this program, the following libraries are used:
 
 - Data.List: To perform list-specific actions lilke intersect.
 - LTS: To be able to use the IOLTS and related types and functions.
+- import Exercise2 - to access gprevious enerators. 
 
 --------- CODE DESCRIPTION (PURPOSE, FEATURES, ARCHITECTURE) ---------
 
+The function after is designed to return the set of states reached after the given trace. 
+To implement after, we use two auxiliary functions - nextStates' and after'. 
+The function nextStates' retrieves all s2 states from all transitions of form (s1, label, s2).
+The function after' recurces over labels a trace to find all states reached after that label. 
+The function after then just 'launches' the process and removes duplicates from the final list.
 
---------- TESTING APPROACH ---------
+--------- TESTING APPROACH -----------
 
--- ghci> quickCheck  prop_identity                 
+To test the after function, we tested a number of different
+properties. All properties are of type Bool, since they all use generators to generate relevant test
+cases. The following properties were checked:
+
+1. Reachability Property - for every state s in the result of after iolts trace,
+there exists a trace from the initial state to s that matches the given trace.
+2. Identity Property - if the trace is empty, the result of after iolts []
+should be a list containing only the initial state. 
+3. Consistency with Single-Step Transitions - compare the result of after with the result of applying a single labeled transition. 
+
+I was only able to implement the identity property and test it using quickCheck. The tests passed.
+
+quickCheck  prop_identity                 
 -- +++ OK, passed 100 tests.
+
+To test the other properties, I would use the following format:
+--quickCheck $ forAll ltsGen $ prop_reachability
+
+Traces and straces would be tested for as they would be included in the properties functions.
+
