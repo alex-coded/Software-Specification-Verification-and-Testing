cef6442723b4dd01021bbf968cb126d0691eaf9f
CH-YSQ <112876130+CH-YSQ@users.noreply.github.com>
2023-09-29 12:46:41 +0200

    comment exercise 3

--- Lab2_shuqi/Exercise3.hs
+++ Lab2_shuqi/Exercise3.hs
@@ -1,7 +1,7 @@
 -- Study: MSc Software Engineering.
--- This program is intended to
+-- This program is intended to implement the function straces that returns all suspension traces of a given IOLTS.
 -- Time spent: 180 min
--- Additional dependencies
+-- Additional dependencies:
 
 module Exercise3 where
 
@@ -10,13 +10,21 @@ import LTS
 import Test.QuickCheck
 
 -- This function returns True if a state in a list of state transitions is quiescent and False otherwise.
+-- This function checks the State has an ouput label. If a state has an output label, this state is not a
+-- quiescent and otherwise, this state is a quiescent.
 isQuiescent :: [(State, Label)] -> [Label] -> Bool
 isQuiescent nt lu = not (any (\lable -> elem lable (map snd nt)) lu)
 
+-- This function finds suspension traces, adds these traces as transitions in the IOLTS and
+-- returns the resulting transition list.
+-- This function checks every state stored in the IOLTS whether the state is a quiescent,
+-- if a state is a quiescent, a transition with delta label will be added to the list of
+-- transitions with the quiescent being the input and the output state.
 straces' :: IOLTS -> [LabeledTransition]
 straces' ([], _, _, t, _) = nub t
 straces' ((qh:qt), li, lu, t, q0) | isQuiescent (nextTransitions' t qh) lu = straces' (qt, li, lu, t ++ [(qh, delta, qh)], q0)
                                   | otherwise = straces' (qt, li, lu, t, q0)
 
+-- This function returns all suspension traces of a given IOLTS.
 straces :: IOLTS -> [Trace]
-straces (q, li, lu, t, q0) = nub $ map snd (traces' (t ++ (straces' (q, li, lu, t, q0))) [([q0],[])])
+straces (q, li, lu, t, q0) = map snd (traces' (straces' (q, li, lu, t, q0)) [([q0],[])])
--- Lab2_shuqi/Exercise4.hs
+++ Lab2_shuqi/Exercise4.hs
@@ -17,8 +17,23 @@ import Data.List
 import LTS
 import Test.QuickCheck
 
+-- nextTransitions':: [LabeledTransition]->State->[(State,Label)]
+-- nextTransitions' lt q0 =  [(s',l) | (s,l,s')<- lt , s == q0]
+
+-- findfollowingtransitions':: [LabeledTransition] -> [State] -> [Label] -> [([State],[Label])]
+-- findfollowingtransitions' lt st ls = [(s:st,ls++[l])| (s,l)<-nextTransitions' lt (head st)]
+
+-- traces':: [LabeledTransition] -> [([State],[Label])]-> [([State],[Label])]
+-- traces' lt [] = []
+-- traces' lt pairs = pairs ++ traces' lt next
+--     where next = concatMap (uncurry $ findfollowingtransitions' lt) pairs
+
+-- traces :: LTS -> [Trace] -- [[Label]]
+-- traces (q, l, lt, q0) = nub $ map snd (traces' lt [([q0],[])])
+
 after :: IOLTS -> Trace -> [State]
-after =
+after ([], _, _, t, _) =
+after ((qh:qt), li, lu, t, q0) =
 
 
 
