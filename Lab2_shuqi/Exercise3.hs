e1830f21bcb880221a7b6339600e8c878c2819bd
Alexandra Volentir <67002616+ada-boost@users.noreply.github.com>
2023-09-29 16:26:02 +0200

    Update Exercise3.hs

--- Lab2_shuqi/Exercise3.hs
+++ Lab2_shuqi/Exercise3.hs
@@ -5,6 +5,7 @@
 
 module Exercise3 where
 
+-- import Exercise2
 import Data.List
 import LTS
 import Test.QuickCheck
@@ -13,7 +14,7 @@ import Test.QuickCheck
 -- This function checks the State has an ouput label. If a state has an output label, this state is not a
 -- quiescent and otherwise, this state is a quiescent.
 isQuiescent :: [(State, Label)] -> [Label] -> Bool
-isQuiescent nt lu = not (any (\lable -> elem lable (map snd nt)) lu)
+isQuiescent nt lu = not (any (\lable -> lable `elem` map snd nt) lu)
 
 -- This function finds suspension transitions, adds these transitions in the list of transitions stored in
 -- IOLTS and returns the resulting transition list.
@@ -24,7 +25,7 @@ isQuiescent nt lu = not (any (\lable -> elem lable (map snd nt)) lu)
 -- transitions with the quiescent being the input and the output state.
 straces' :: IOLTS -> [LabeledTransition]
 straces' ([], _, _, t, _) = nub t
-straces' ((qh:qt), li, lu, t, q0) | isQuiescent (nextTransitions' t qh) lu = straces' (qt, li, lu, t ++ [(qh, delta, qh)], q0)
+straces' (qh:qt, li, lu, t, q0) | isQuiescent (nextTransitions' t qh) lu = straces' (qt, li, lu, t ++ [(qh, delta, qh)], q0)
                                   | otherwise = straces' (qt, li, lu, t, q0)
 
 -- This function returns all suspension traces of a given IOLTS.
@@ -32,3 +33,57 @@ straces' ((qh:qt), li, lu, t, q0) | isQuiescent (nextTransitions' t qh) lu = str
 -- based on the updated transitions including suspension transitions.
 straces :: IOLTS -> [Trace]
 straces (q, li, lu, t, q0) = map snd (traces' (straces' (q, li, lu, t, q0)) [([q0],[])])
+
+
+-- TESTING PART --
+
+isInputLabel :: Label -> Bool
+isInputLabel label = not (null label) && head label == '?'
+
+isOutputLabel :: Label -> Bool
+isOutputLabel label = not (null label) && head label == '!'
+
+
+prop_no_quiescent_states :: IOLTS -> Property
+prop_no_quiescent_states (q, li, lu, t, q0) = not (null (straces (q, li, lu, t, q0))) ==> not (any (hasQuiescent lu) (straces (q, li, lu, t, q0)))
+  where
+    hasQuiescent :: [Label] -> Trace -> Bool
+    hasQuiescent lu = any (\label -> isQuiescent [(0, label)] lu)
+
+
+-- prop_start_from_initial_state :: IOLTS -> Property
+-- prop_start_from_initial_state (q, li, lu, t, q0) = not (null (straces (q, li, lu, t, q0))) ==> all (\(state, _) -> state == q0) (head (straces (q, li, lu, t, q0)))
+
+-- prop_suspended_trace_structure :: IOLTS -> Property
+-- prop_suspended_trace_structure (q, li, lu, t, q0) = not (null (straces (q, li, lu, t, q0))) ==> all isTransitionList (straces (q, li, lu, t, q0))
+--   where
+--     isTransitionList :: Trace -> Bool
+--     isTransitionList trace = all isTransitionLabel trace
+
+--     isTransitionLabel :: Label -> Bool
+--     isTransitionLabel label = label /= tau && (isInputLabel label || isOutputLabel label)
+
+
+-- This generator randomly generates transitions of the form (State, Label, State).
+transitionGen :: Gen LabeledTransition
+transitionGen = do
+    k <- chooseInteger(2, 20)
+    s <- chooseInteger(1, k)
+    t <- suchThat (chooseInteger(1, k)) (/= s)
+    l <- elements ([tau] ++ (map (\ x -> if mod x 2 == 0 then "?" ++ (show x) else "!" ++ (show x)) [1..k]))
+    return (s, l, t)
+
+-- This generator randomly generates valid IOLTS using the createIOLTS function with randomly generated transitions.
+ltsGen :: Gen IOLTS
+ltsGen = do
+    k <- choose(2, 20)
+    n <- choose(2, k)
+    transitions <- vectorOf n transitionGen
+    return (createIOLTS transitions)
+
+
+
+main :: IO ()
+main = do
+    putStrLn "Property 1: Suspended traces must not contain quiescent states."
+    quickCheck $ forAll ltsGen $ prop_no_quiescent_states
