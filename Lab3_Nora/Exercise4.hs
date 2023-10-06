05e9b77e7bb6de1c6782222ad3c82bd8a546971e
norasilven <norasilven@gmail.com>
2023-10-06 02:41:29 +0200

    Commented ex 1 and 2, halfway done with commenting ex 4

--- Lab3_Nora/Exercise4.hs
+++ Lab3_Nora/Exercise4.hs
@@ -1,5 +1,8 @@
+-- Study: MSc Software Engineering.
+-- This program is intended to calculate the strength of a set of properties.
+-- Time spent: 2 hours
+
 module Exercise4 where
-import Data.List
 import Test.QuickCheck
 import Mutation
 import MultiplicationTable
@@ -18,6 +21,34 @@ strengthPerc n prop fun mutator | n == 0 = return (0)
                                     killed <- countKilled n prop fun mutator
                                     return ((div killed n) * 100)
 
+-- Do a simple check to see if the strength of all multiplicationTable properties is correct.
 main :: IO Int
 main = do
-    strengthPerc 100 multiplicationTableProps multiplicationTable addElements
+    strengthPerc 4000 multiplicationTableProps multiplicationTable addElements
+
+
+{-
+--------- LIBRARIES ---------
+
+This program uses the following libraries:
+- Test.QuickCheck: To be able to use the Gen construct for the mutators.
+- Mutation: To be able to use the standard mutators.
+- MultiplicationTable: To be able to use the properties and function for checking.
+- Exercise 2 (countSurvivors): Access to the countSurvivors function.
+
+--------- CODE DESCRIPTION (PURPOSE, FEATURES, ARCHITECTURE) ---------
+
+This program is intended to calculate the strength of a set of properties. The strength is defined as
+the percentage of killed mutations. So we need to count the number of failed mutants and compare it with
+the total number of mutants. I implemented this by counting the number of survivors
+(using the countSurvivors function) and subtracting that from the total number of mutants in order to
+get the number of killed mutants. These killed mutants include mutants that did not actually change the
+output, which is correct according to my classification described in Exercise2.hs. When I have the number
+of killed mutants, I divide this mumber by the total number of mutants and multiply by 100, to get the
+percentage of killed mutants.
+
+--------- IMPORTANCE OF STRENGTH ---------
+
+...
+
+-}
