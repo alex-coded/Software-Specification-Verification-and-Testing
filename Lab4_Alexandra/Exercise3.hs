8b2af7881e510168416997d5505fa54a5d8b3867
Alexandra Volentir <67002616+ada-boost@users.noreply.github.com>
2023-10-12 12:37:49 +0200

    Ex3 oneliner

--- Lab4_Alexandra/Exercise3.hs
+++ Lab4_Alexandra/Exercise3.hs
@@ -2,24 +2,9 @@ import SetOrd
 
 type Rel a = Set (a, a)
 
-insertList :: (Ord a) => a -> [a] -> [a]
-insertList x [] = [x]
-insertList x ys@(y:ys') = case compare x y of
-    GT -> y : insertList x ys'
-    EQ -> ys
-    _  -> x : ys
-
--- Add reverse of each pair to R
-addRev :: Ord a => Rel a -> Rel a
-addRev (Set pairs) = Set (pairs ++ [(y, x) | (x, y) <- pairs])
-
--- Remove duplicates
-getRidOfDups :: Ord a => Rel a -> Rel a
-getRidOfDups (Set pairs) = Set (foldr insertList [] pairs)
-
 -- Symmetric closure 
 symClos :: Ord a => Rel a -> Rel a
-symClos rel = getRidOfDups (addRev rel)
+symClos (Set pairs) = Set (pairs ++ [(y, x) | (x, y) <- pairs])
 
 main :: IO ()
 main = do
