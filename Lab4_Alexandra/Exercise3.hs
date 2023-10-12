b606efd697274aebeb185e007d783f5d3b171212
norasilven <norasilven@gmail.com>
2023-10-12 14:47:51 +0200

    no changes

--- Lab4_Alexandra/Exercise3.hs
+++ Lab4_Alexandra/Exercise3.hs
@@ -2,7 +2,7 @@ import SetOrd
 
 type Rel a = Set (a, a)
 
--- Symmetric closure 
+-- Symmetric closure
 symClos :: Ord a => Rel a -> Rel a
 symClos (Set pairs) = Set (pairs ++ [(y, x) | (x, y) <- pairs])
 
